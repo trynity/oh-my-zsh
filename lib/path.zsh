8ae8ccb0d82473b78b080bb795d83481ce0c42a9
Trynity Mirell <trynity@mirell.org>
2013-04-04 08:12:46 -0500

    Create a lib/path.zsh to keep all the PATH stuff nicely in one place

---
+++ lib/path.zsh
@@ -0,0 +1,20 @@
+# Add /usr/local/[s]bin to PATH
+path=(/usr/local/bin /usr/local/sbin $path)
+
+# Add Haskell's cabal bin directory to PATH
+path=(${HOME}/.cabal/bin $path)
+
+# Add Homebrew npm bin directory to PATH
+path=(/usr/local/share/npm/bin $path)
+
+# Add Homebrew python executables to PATH
+path=(/usr/local/share/python $path)
+
+# TexLive
+(($+commands[texdist])) && path+=$(texdist --current --expand)/bin/universal-darwin
+
+# jenv
+path=(${HOME}/.jenv/bin $path)
+
+# Local bin directory
+path=(${HOME}/.bin $path)
