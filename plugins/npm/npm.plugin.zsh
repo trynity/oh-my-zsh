8ab070580cac39699404e86ff3c610d0874c67d6
Trynity Mirell <trynity@mirell.org>
2013-06-09 18:51:24 -0500

    Add "npm exec" a la "bundle exec" for node projects

--- plugins/npm/npm.plugin.zsh
+++ plugins/npm/npm.plugin.zsh
@@ -13,3 +13,13 @@ alias npms="npm i -S "
 
 # Install and save to dev-dependencies
 alias npmd="npm i -D "
+
+# npm exec : For "bundler exec" similar functionality
+function npm() {
+	if [[ $1 == "exec" ]]; then
+		PATH="$(command npm bin):$PATH" $*[2,-1];
+	else
+		command npm $*[2,-1];
+	fi;
+}
+>>>>>>> Add "npm exec" a la "bundle exec" for node projects
