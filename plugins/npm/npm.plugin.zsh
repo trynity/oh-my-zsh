342b23a92f320ab3be44023973d114b57e60ce70
Trynity Mirell <trynity@mirell.org>
2013-06-09 18:51:24 -0500

    Add "npm exec" a la "bundle exec" for node projects

--- plugins/npm/npm.plugin.zsh
+++ plugins/npm/npm.plugin.zsh
@@ -1,1 +1,10 @@
 eval "$(npm completion 2>/dev/null)"
+
+# npm exec : For "bundler exec" similar functionality
+function npm() {
+	if [[ $1 == "exec" ]]; then
+		PATH="$(command npm bin):$PATH" $*[2,-1];
+	else
+		command npm $*[2,-1];
+	fi;
+}
