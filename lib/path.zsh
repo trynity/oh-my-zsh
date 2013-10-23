b34365bf88dedfb8d52586b55ef86abffd673e31
Trynity Mirell <trynity@mirell.org>
2013-10-23 03:47:45 -0500

    path.zsh: Cleaned up and reordered nvm/rvm/phpbrew/perlbrew/jenv in PATH

--- lib/path.zsh
+++ lib/path.zsh
@@ -4,11 +4,11 @@ path=(/usr/local/bin /usr/local/sbin $path)
 # Add Haskell's cabal bin directory to PATH
 path=(${HOME}/.cabal/bin $path)
 
+# Add Haskell Platform bin directory to PATH
+path=(${HOME}/Library/Haskell/bin $path)
+
 # Add Homebrew npm bin directory to PATH
 path=(/usr/local/share/npm/bin $path)
-
-# Add Homebrew python executables to PATH
-path=(/usr/local/share/python $path)
 
 # XQuartz
 path=(/opt/X11/bin $path)
@@ -18,9 +18,35 @@ path=(/opt/X11/bin $path)
 
 # jenv
 path=(${HOME}/.jenv/bin $path)
+eval "$(jenv init - zsh)"
 
-# Local bin directory
+# Local bin directoies
 path=(${HOME}/.bin $path)
+path=(${HOME}/.local/bin $path)
 
 # ccache for Homebrew
 [[ -e /usr/local/bin/brew ]] && path=($(brew --prefix)/Library/LinkedKegs/ccache/libexec $path)
+
+# nvm/rvm/perlbrew path reodering
+nvm_paths=()
+rvm_paths=()
+perlbrew_paths=()
+phpbrew_paths=()
+jenv_paths=()
+for i in $path; do
+	if [[ $i  =~ ".nvm" ]]; then
+		nvm_paths+=$i
+	elif [[ $i =~ ".rvm" ]]; then
+		rvm_paths+=$i
+	elif [[ $i =~ $PERLBREW_ROOT ]]; then
+		perlbrew_paths+=$i
+	elif [[ $i =~ ".phpbrew" ]]; then
+		phpbrew_paths+=$i
+	elif [[ $i =~ ".jenv" ]]; then
+		jenv_paths+=$i
+	fi
+done
+
+path=($rvm_paths $nvm_paths $perlbrew_paths $phpbrew_paths $jenv_paths $path)
+
+typeset -U path
